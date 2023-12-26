4607c2d90fe38c83810b9e5cda07ee38006e876d
bernardocastro95 <bernardoc544@gmail.com>
2023-12-26 17:09:33 -0500

    initializing layout

--- app/src/main/java/com/example/bookhandler/MyDatabaseHelper.java
+++ app/src/main/java/com/example/bookhandler/MyDatabaseHelper.java
@@ -1,4 +1,23 @@
 package com.example.bookhandler;
 
-public class MyDatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+    public MyDatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
