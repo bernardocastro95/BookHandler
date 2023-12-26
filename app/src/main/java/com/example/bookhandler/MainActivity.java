4392c6c081c1739b90e355c36cfc62ea559ab58b
bernardocastro95 <bernardoc544@gmail.com>
2023-12-26 17:36:52 -0500

    new activity created

--- app/src/main/java/com/example/bookhandler/MainActivity.java
+++ app/src/main/java/com/example/bookhandler/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.bookhandler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycleView);
+        fab = findViewById(R.id.add_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
